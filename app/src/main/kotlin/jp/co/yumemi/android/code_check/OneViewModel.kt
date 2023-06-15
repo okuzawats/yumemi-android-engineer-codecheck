8ac2b910fe507dc8af0c7a4a522f0c9722c3aaa8
okuzawats <okuzawats@gmail.com>
2023-06-15 23:18:07 +0900

    ./gradlew ktlintFormatの実行

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -23,65 +23,72 @@ import java.util.*
  * TwoFragment で使う
  */
 class OneViewModel(
-    val context: Context
+  val context: Context
 ) : ViewModel() {
 
-    // 検索結果
-    fun searchResults(inputText: String): List<item> = runBlocking {
-        val client = HttpClient(Android)
+  // 検索結果
+  fun searchResults(inputText: String): List<item> = runBlocking {
+    val client = HttpClient(Android)
 
-        return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
+    return@runBlocking GlobalScope.async {
+      val response: HttpResponse = client?.get(
+        "https://api.github.com/search/repositories"
+      ) {
+        header("Accept", "application/vnd.github.v3+json")
+        parameter("q", inputText)
+      }
 
-            val jsonBody = JSONObject(response.receive<String>())
+      val jsonBody = JSONObject(response.receive<String>())
 
-            val jsonItems = jsonBody.optJSONArray("items")!!
+      val jsonItems = jsonBody.optJSONArray("items")!!
 
-            val items = mutableListOf<item>()
+      val items = mutableListOf<item>()
 
-            /**
-             * アイテムの個数分ループする
-             */
-            for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)!!
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
+      /**
+       * アイテムの個数分ループする
+       */
+      for (i in 0 until jsonItems.length()) {
+        val jsonItem = jsonItems.optJSONObject(i)!!
+        val name = jsonItem.optString("full_name")
+        val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString(
+          "avatar_url"
+        )
+        val language = jsonItem.optString("language")
+        val stargazersCount = jsonItem.optLong("stargazers_count")
+        val watchersCount = jsonItem.optLong("watchers_count")
+        val forksCount = jsonItem.optLong("forks_conut")
+        val openIssuesCount = jsonItem.optLong("open_issues_count")
 
-                items.add(
-                    item(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
-                        language = context.getString(R.string.written_language, language),
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount
-                    )
-                )
-            }
+        items.add(
+          item(
+            name = name,
+            ownerIconUrl = ownerIconUrl,
+            language = context.getString(
+              R.string.written_language,
+              language
+            ),
+            stargazersCount = stargazersCount,
+            watchersCount = watchersCount,
+            forksCount = forksCount,
+            openIssuesCount = openIssuesCount
+          )
+        )
+      }
 
-            lastSearchDate = Date()
+      lastSearchDate = Date()
 
-            return@async items.toList()
-        }.await()
-    }
+      return@async items.toList()
+    }.await()
+  }
 }
 
 @Parcelize
 data class item(
-    val name: String,
-    val ownerIconUrl: String,
-    val language: String,
-    val stargazersCount: Long,
-    val watchersCount: Long,
-    val forksCount: Long,
-    val openIssuesCount: Long,
+  val name: String,
+  val ownerIconUrl: String,
+  val language: String,
+  val stargazersCount: Long,
+  val watchersCount: Long,
+  val forksCount: Long,
+  val openIssuesCount: Long
 ) : Parcelable
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/TwoFragment.kt
@@ -14,26 +14,26 @@ import jp.co.yumemi.android.code_check.databinding.FragmentTwoBinding
 
 class TwoFragment : Fragment(R.layout.fragment_two) {
 
-    private val args: TwoFragmentArgs by navArgs()
+  private val args: TwoFragmentArgs by navArgs()
 
-    private var binding: FragmentTwoBinding? = null
-    private val _binding get() = binding!!
+  private var binding: FragmentTwoBinding? = null
+  private val _binding get() = binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        Log.d("検索した日時", lastSearchDate.toString())
+    Log.d("検索した日時", lastSearchDate.toString())
 
-        binding = FragmentTwoBinding.bind(view)
+    binding = FragmentTwoBinding.bind(view)
 
-        var item = args.item
+    var item = args.item
 
-        _binding.ownerIconView.load(item.ownerIconUrl);
-        _binding.nameView.text = item.name;
-        _binding.languageView.text = item.language;
-        _binding.starsView.text = "${item.stargazersCount} stars";
-        _binding.watchersView.text = "${item.watchersCount} watchers";
-        _binding.forksView.text = "${item.forksCount} forks";
-        _binding.openIssuesView.text = "${item.openIssuesCount} open issues";
-    }
+    _binding.ownerIconView.load(item.ownerIconUrl)
+    _binding.nameView.text = item.name
+    _binding.languageView.text = item.language
+    _binding.starsView.text = "${item.stargazersCount} stars"
+    _binding.watchersView.text = "${item.watchersCount} watchers"
+    _binding.forksView.text = "${item.forksCount} forks"
+    _binding.openIssuesView.text = "${item.openIssuesCount} open issues"
+  }
 }

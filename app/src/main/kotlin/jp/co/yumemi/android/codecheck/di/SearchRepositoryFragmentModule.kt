d55a3cfaaae127ea28bb81e3a6478247dcb7ebb9
okuzawats <okuzawats@gmail.com>
2023-06-26 14:21:52 +0900

    SearchRepoistoryModuleを追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/di/SearchRepositoryFragmentModule.kt
@@ -0,0 +1,41 @@
+package jp.co.yumemi.android.codecheck.di
+
+import androidx.fragment.app.Fragment
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.FragmentScoped
+import jp.co.yumemi.android.codecheck.feature.search.SearchRepositoryContract
+import jp.co.yumemi.android.codecheck.feature.search.SearchRepositoryFragment
+import jp.co.yumemi.android.codecheck.feature.search.SearchRepositoryNavigator
+import jp.co.yumemi.android.codecheck.feature.search.SearchRepositoryPresenter
+import jp.co.yumemi.android.codecheck.navigation.DefaultSearchRepositoryNavigator
+
+@Module
+@InstallIn(FragmentComponent::class)
+class SearchRepositoryFragmentModule {
+  @Provides
+  @FragmentScoped
+  fun provideSearchRepositoryFragment(
+    fragment: Fragment,
+  ): SearchRepositoryFragment = fragment as SearchRepositoryFragment
+
+  @Provides
+  @FragmentScoped
+  fun provideSearchRepositoryView(
+    searchRepositoryFragment: SearchRepositoryFragment,
+  ): SearchRepositoryContract.View = searchRepositoryFragment
+
+  @Provides
+  @FragmentScoped
+  fun provideSearchRepositoryPresenter(
+    searchRepositoryPresenter: SearchRepositoryPresenter,
+  ): SearchRepositoryContract.Presenter = searchRepositoryPresenter
+
+  @Provides
+  @FragmentScoped
+  fun provideSearchRepositoryNavigator(
+    searchRepositoryNavigator: DefaultSearchRepositoryNavigator,
+  ): SearchRepositoryNavigator = searchRepositoryNavigator
+}

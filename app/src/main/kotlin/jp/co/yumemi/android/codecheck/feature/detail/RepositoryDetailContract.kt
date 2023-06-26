6ce65597d510a03b179e1db0bb40d85d71f1bb47
okuzawats <okuzawats@gmail.com>
2023-06-26 17:46:27 +0900

    showRepositoryの実装

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/detail/RepositoryDetailContract.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/detail/RepositoryDetailContract.kt
@@ -1,5 +1,7 @@
 package jp.co.yumemi.android.codecheck.feature.detail
 
+import jp.co.yumemi.android.codecheck.Repository
+
 /**
  * リポジトリ詳細画面の契約
  */
@@ -7,7 +9,12 @@ interface RepositoryDetailContract {
   /**
    * リポジトリ詳細画面のViewを定義するinterface
    */
-  interface View
+  interface View {
+    /**
+     * リポジトリの詳細を表示する
+     */
+    fun showRepository(repository: Repository)
+  }
 
   /**
    * リポジトリ詳細画面のPresenterを定義するinterface
--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/detail/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/detail/RepositoryDetailFragment.kt
@@ -6,10 +6,10 @@ package jp.co.yumemi.android.codecheck.feature.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.navArgs
 import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.codecheck.R
+import jp.co.yumemi.android.codecheck.Repository
 import jp.co.yumemi.android.codecheck.databinding.FragmentRepositoryDetailBinding
 
 @AndroidEntryPoint
@@ -17,17 +17,15 @@ class RepositoryDetailFragment :
   Fragment(R.layout.fragment_repository_detail),
   RepositoryDetailContract.View {
 
-  private val args: RepositoryDetailFragmentArgs by navArgs()
-
   private var _binding: FragmentRepositoryDetailBinding? = null
   private val binding: FragmentRepositoryDetailBinding
     get() = checkNotNull(_binding)
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     _binding = FragmentRepositoryDetailBinding.bind(view)
+  }
 
-    val repository = args.repository
-
+  override fun showRepository(repository: Repository) {
     binding.ownerIconView.load(repository.ownerIconUrl)
     binding.nameView.text = repository.name
     binding.languageView.text = repository.language

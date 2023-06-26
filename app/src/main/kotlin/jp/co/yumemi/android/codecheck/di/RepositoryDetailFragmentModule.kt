66acac647d6e983f877c21d675a097eca372ce29
okuzawats <okuzawats@gmail.com>
2023-06-26 21:42:26 +0900

    NavArgsをDIするように修正

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/di/RepositoryDetailFragmentModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/di/RepositoryDetailFragmentModule.kt
@@ -1,6 +1,8 @@
 package jp.co.yumemi.android.codecheck.di
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgsLazy
+import androidx.navigation.fragment.navArgs
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,6 +10,7 @@ import dagger.hilt.android.components.FragmentComponent
 import dagger.hilt.android.scopes.FragmentScoped
 import jp.co.yumemi.android.codecheck.feature.detail.RepositoryDetailContract
 import jp.co.yumemi.android.codecheck.feature.detail.RepositoryDetailFragment
+import jp.co.yumemi.android.codecheck.feature.detail.RepositoryDetailFragmentArgs
 import jp.co.yumemi.android.codecheck.feature.detail.RepositoryDetailProvider
 import jp.co.yumemi.android.codecheck.feature.detail.RepositoryDetailProviderImpl
 
@@ -28,6 +31,14 @@ class RepositoryDetailFragmentModule {
     fragment: RepositoryDetailFragment,
   ): RepositoryDetailContract.View {
     return fragment
+  }
+
+  @Provides
+  @FragmentScoped
+  fun provideNavArgs(
+    fragment: RepositoryDetailFragment,
+  ): NavArgsLazy<RepositoryDetailFragmentArgs> {
+    return fragment.navArgs()
   }
 
   @Provides

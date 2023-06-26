99725efd4bbc24a11c57fb029165dded6107de0d
okuzawats <okuzawats@gmail.com>
2023-06-26 20:29:48 +0900

    [refactor] SearchRepositoryDiffUtilProviderのリフォーマット (#62)

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/search/SearchRepositoryDiffUtilProvider.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/search/SearchRepositoryDiffUtilProvider.kt
@@ -9,11 +9,17 @@ import jp.co.yumemi.android.codecheck.Repository
 class SearchRepositoryDiffUtilProvider {
   fun provide(): DiffUtil.ItemCallback<Repository> {
     return object : DiffUtil.ItemCallback<Repository>() {
-      override fun areItemsTheSame(oldItem: Repository, newItem: Repository): Boolean {
+      override fun areItemsTheSame(
+        oldItem: Repository,
+        newItem: Repository,
+      ): Boolean {
         return oldItem.name == newItem.name
       }
 
-      override fun areContentsTheSame(oldItem: Repository, newItem: Repository): Boolean {
+      override fun areContentsTheSame(
+        oldItem: Repository,
+        newItem: Repository,
+      ): Boolean {
         return oldItem == newItem
       }
     }
